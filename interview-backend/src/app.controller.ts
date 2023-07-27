f4d0d685a2b7caf349c2682d2edc35cabd31a791
Matthew Cwalina <matthewcwalina@gmail.com>
2023-07-27 14:51:43 +0200

    Added a basic get command to retrieve all cities from the JSON and return the parsed data

--- interview-backend/src/app.controller.ts
+++ interview-backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { readFileSync } from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('cities')
+  getCities(): any {
+    const citiesData = readFileSync('../cities.json', 'utf-8');
+    console.log(citiesData);
+    return JSON.parse(citiesData);
   }
 }
